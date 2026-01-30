9761adfbd7f44886765d431df792399d78893573
SohamB4746Y <sohambhattacharya023@gmail.com>
2026-01-30 10:03:05 +0530

    Initial Commit

---
+++ SwiftStudentChallengeDemo/SwiftStudentChallengeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudentChallengeDemoApp.swift
+//  SwiftStudentChallengeDemo
+//
+//  Created by Soham Bhattacharya on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudentChallengeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
